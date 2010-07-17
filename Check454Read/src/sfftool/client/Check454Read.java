c67d4cf39f9631b1cbaab839733bf9a37f1dc386
sharma.animesh <sharma.animesh@782fc89d-aff7-cfad-3c7f-37fe626a450f>
2010-07-17 20:29:59 +0000

    Check 454 Reads for Assembly

---
+++ Check454Read/src/sfftool/client/Check454Read.java
@@ -0,0 +1,10 @@
+package sfftool.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+
+public class Check454Read implements EntryPoint {
+	public void onModuleLoad() {
+	}
+}
